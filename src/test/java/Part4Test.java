9cc4d87155559a4fc98f664921bd2a7e2d258165
ZOkrutnyi <leto6601@gmail.com>
2021-05-25 12:02:47 +0300

    all tasks

---
+++ src/test/java/Part4Test.java
@@ -0,0 +1,24 @@
+import com.delphi.app.Part4;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Part4Test {
+    Part4 part4 = new Part4();
+    @Test
+    public void validConnString()
+    {
+        Assert.assertEquals("Port: localhost\n" +
+                "Instance name: MyTest\n" +
+                "IP: 8674", part4.connData("localhost\\MyTest,8674"));
+    }
+    @Test
+    public void inputNull()
+    {
+        Assert.assertEquals("Invalid connection type", part4.connData(null));
+    }
+    @Test
+    public void inputInvalidData()
+    {
+        Assert.assertEquals("Invalid connection type", part4.connData("I think this is ip"));
+    }
+}
