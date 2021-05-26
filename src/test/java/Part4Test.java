52c18bc11dedbd387094ec26934f59cc9cb9e364
ZOkrutnyi <leto6601@gmail.com>
2021-05-26 11:13:48 +0300

    task 4 tests

--- src/test/java/Part4Test.java
+++ src/test/java/Part4Test.java
@@ -6,10 +6,22 @@ public class Part4Test {
     Part4 part4 = new Part4();
 
     @Test
-    public void validConnString() {
-        Assert.assertEquals("Port: localhost\n" +
-                "Instance name: MyTest\n" +
-                "IP: 8674", part4.connData("localhost\\MyTest,8674"));
+    public void fullConnString() {
+        Assert.assertEquals("IP: localhost\n" +
+                "Name: MyTest\n" +
+                "Port: 8674\n", part4.connData("localhost\\MyTest,8674"));
+    }
+    @Test
+    public void onlyIPConnString() {
+        Assert.assertEquals("IP: localhost\n" +
+                "Name: defaultServerName\n" +
+                "Port: 8080\n", part4.connData("localhost\\"));
+    }
+    @Test
+    public void IPNameConnString() {
+        Assert.assertEquals("IP: localhost\n" +
+                "Name: interpol\n" +
+                "Port: 8080\n", part4.connData("localhost\\interpol"));
     }
 
     @Test
