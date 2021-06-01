3815644762c793d4ec77f9bca09b0ed877487c08
ZOkrutnyi <leto6601@gmail.com>
2021-06-01 12:59:51 +0300

    refactored

--- src/main/java/com/delphi/app/Part4.java
+++ src/main/java/com/delphi/app/Part4.java
@@ -10,38 +10,28 @@ public class Part4 {
     private String name = null;
     private String port = null;
     public String connData(final String conn) {
-        if (conn == null) {
-            return "Invalid connection type";
+        if (conn == null || conn.isEmpty()) {
+            return "Invalid connection type\n";
         }
-
-        Pattern global = Pattern.compile("^(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\w+)|([A-z]+)|(\\d{4})");
+        Pattern global = Pattern.compile("([\\w\\.\\-\\_]+)\\\\?(\\w*)\\,?(.*)?");
         Matcher matcher = global.matcher(conn);
-        while (matcher.find()) {
-            if (matcher.group(1) != null)
-                this.ip = matcher.group(1);
-            if (matcher.group(2) != null)
-                this.name = matcher.group(2).replace("\\", "");
-            if (matcher.group(3) != null)
-                this.port = matcher.group(3);
+        if (matcher.find()) {
+                ip = matcher.group(1).isEmpty() ? "unknown" : matcher.group(1);
+                name = matcher.group(2).isEmpty() ? "defaultServerName": matcher.group(2);
+                port = matcher.group(3).isEmpty() ? "8080": matcher.group(3);
         }
-        if (this.ip == null)
-            return "Invalid connection type";
-        if (this.name == null)
-            this.name = "defaultServerName";
-        if (this.port == null)
-            this.port = "8080";
-        return "IP: " + this.ip + "\nName: " + this.name + "\nPort: " + this.port + "\n";
-
+                return "IP: " + ip + "\nName: " + name + "\nPort: " + port + "\n";
     }
 
     public static void main(String[] args) {
         Part4 p = new Part4();
         System.out.println(p.connData("localhost\\MyTest,8674"));
-        System.out.println(p.getIP());
-//        System.out.println(new Part4().connData("192.164.131.131\\MyTest,8674"));
-//        System.out.println(new Part4().connData("localhost"));
-//        System.out.println(new Part4().connData("localhost\\MyTest"));
-//        System.out.println(new Part4().connData("localhost,4321"));
+        System.out.println(new Part4().connData("192_164-131_131\\MyTest,8674"));
+        System.out.println(new Part4().connData("localhost"));
+        System.out.println(new Part4().connData(null));
+        System.out.println(new Part4().connData("localhost\\MyTest"));
+        System.out.println(new Part4().connData("localhost,4321"));
+        System.out.println(p.getName());
     }
     public String getIP()
     {
